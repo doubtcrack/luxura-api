6c06aaa54e309e7d09445f195b3c49b82c1a02fb
doubtcrack <1.edujock@gmail.com>
2024-09-19 18:07:56 +0530

    cors again

--- app.js
+++ app.js
@@ -37,16 +37,16 @@ app.use(compression());
 //   })
 // );
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "http://localhost:3000",
-    "https://luxurafootwear.pages.dev/",
-  ]); // Replace with your frontend URL
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", [
+//     "http://localhost:3000",
+//     "https://luxurafootwear.pages.dev/",
+//   ]); // Replace with your frontend URL
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 // Mount routes
 app.use("/api/users", authRoutes);
 app.use("/api", routes);
